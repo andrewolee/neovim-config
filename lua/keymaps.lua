eecaf7c1419f733add4834a896e841dcece351db
Andrew O. Lee <andrewolee99@gmail.com>
2023-02-04 21:39:52 -0800

    cleanup

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,6 @@
+-- Set leader
+vim.g.mapleader = " "
+
 -- Don't yank with x
 vim.keymap.set('n', 'x', '"_x')
 
@@ -16,3 +19,6 @@ vim.keymap.set('n', 'sh', '<C-w>h')
 vim.keymap.set('n', 'sj', '<C-w>j')
 vim.keymap.set('n', 'sk', '<C-w>k')
 vim.keymap.set('n', 'sl', '<C-w>l')
+
+-- Netrw
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
