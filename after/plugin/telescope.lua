01bd2ab63dbf5fc1d6fcd961a00a3214cfa1a13a
Andrew O. Lee <andrewolee99@gmail.com>
2023-02-04 23:10:38 -0800

    telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,15 @@
 local builtin = require('telescope.builtin')
 
+require('telescope').setup{
+    pickers = {
+        git_files = {
+            theme = 'dropdown',
+        },
+        live_grep = {
+            theme = 'dropdown',
+        }
+    }
+}
+
 vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
