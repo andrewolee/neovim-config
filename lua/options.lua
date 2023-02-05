4d726b150e6527851695eed3a12378018f401f80
Andrew O. Lee <andrewolee99@gmail.com>
2023-02-04 21:08:41 -0800

    options

--- lua/options.lua
+++ lua/options.lua
@@ -5,30 +5,33 @@ vim.scriptencoding = 'utf-8'
 
 -- Editor options
 vim.opt.title = true
-
 vim.opt.number = true
 vim.opt.mouse = 'a'
-vim.opt.cursorline = true
 vim.opt.scrolloff = 5
 vim.opt.wrap = true
 vim.opt.breakindent = true
 vim.opt.laststatus = 2
 vim.opt.signcolumn = 'yes'
-
+vim.opt.completeopt = { 'menuone', 'noselect' }
 vim.opt.showcmd = true
+
+-- Tab options
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 vim.opt.smarttab = true
 vim.opt.smartindent = true
 vim.opt.backspace = { 'indent', 'eol', 'start' }
 
+-- Search options
 vim.opt.hlsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.completeopt = { 'menuone', 'noselect' }
 
+-- Color options
 vim.opt.termguicolors = true
+
+-- Split options
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
